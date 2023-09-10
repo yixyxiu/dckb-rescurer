e4927d5e85481339958d6e6966b63346c42d8a52
phroi <90913182+phroi@users.noreply.github.com>
2023-09-10 16:01:12 +0000

    Add html fallback placeholder

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 import { App } from "./App";
 
+const placeholder = document.getElementById("app-placeholder")!;
 const container = document.getElementById("app")!;
 const root = createRoot(container)
+placeholder.remove();
 root.render(<App />);
