dd4e782b04734b2d75606c9a96d758def05429fe
phroi <90913182+phroi@users.noreply.github.com>
2023-09-11 15:07:20 +0000

    Add dummy interaction when loading the application

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 import { App } from "./App";
 
-const placeholder = document.getElementById("app-placeholder")!;
+const placeholder = document.getElementById("dummy")!;
 const container = document.getElementById("app")!;
 const root = createRoot(container)
 placeholder.remove();
